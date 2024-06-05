acd7977b9518ad7d580f6ae8e9c4cad9d8c99510
darealZoid <darealZoid@gmail.com>
2024-06-05 13:11:31 +0800

    fixed toggle modal

--- resources/views/livewire/components/UserManagement/user-form.blade.php
+++ resources/views/livewire/components/UserManagement/user-form.blade.php
@@ -11,7 +11,7 @@
                     </h3>
                     <button type="button"
                         class="inline-flex items-center justify-center w-8 h-8 text-sm text-gray-400 bg-transparent rounded-lg hover:bg-gray-200 hover:text-gray-900 ms-auto dark:hover:bg-gray-600 dark:hover:text-white"
-                        data-modal-hide="editUserModal">
+                        data-modal-hide="UserModal">
                         <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                             viewBox="0 0 14 14">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
