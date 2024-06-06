79fe0dc4f23d7359f6af7c22025597e59460aa04
darealZoid <darealZoid@gmail.com>
2024-06-06 08:34:39 +0800

    wala lang

--- resources/views/livewire/components/UserManagement/user-table.blade.php
+++ resources/views/livewire/components/UserManagement/user-table.blade.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 {{-- <div
     class="relative overflow-x-auto overflow-y-auto shadow-md ml-[242px] mr-[22px] sm:rounded-lg border border-black rounded-md">
     <div class="flex flex-row items-center justify-between bg-white dark:bg-gray-900 mx-[12px] my-2">
@@ -7,6 +8,17 @@
                     fill="none" viewBox="0 0 20 20">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+=======
+<div class="relative overflow-x-auto overflow-y-auto shadow-md ml-[242px] mr-[22px] sm:rounded-lg border border-black rounded-md">
+    <div class="flex flex-row items-center justify-between bg-white dark:bg-gray-900] mx-[12px] my-2">
+        <div class="absolute fixed top-[20px]">
+            <div
+                class="absolute inset-y-0 flex items-center pointer-events-none rtl:inset-r-0 start-0 ps-3">
+                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                        stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+>>>>>>> Stashed changes
                 </svg>
             </div>
             <input type="text" id="table-search" wire:model.live.debounce.300ms = "search"
@@ -14,7 +26,13 @@
                 placeholder="Search by Name or Username">
         </div>
 
+<<<<<<< Updated upstream
         <div class="fixed top-[200px] right-[32px]">
+=======
+        <div class="absolute top-[200px] right-[32px]">
+
+
+>>>>>>> Stashed changes
             <div class="flex flex-row gap-2 border border-black">
                 <button class="px-4 py-2 bg-red-100">filter1</button>
                 <button class="px-4 py-2 bg-blue-100">filter2</button>
