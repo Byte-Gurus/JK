414186c151ff2ce745f51af4065273159d2dc1b7
darealZoid <darealZoid@gmail.com>
2024-06-08 17:29:52 +0800

    button and logout styling

--- resources/views/livewire/components/logout.blade.php
+++ resources/views/livewire/components/logout.blade.php
@@ -2,7 +2,7 @@
 
 <div>
     <button data-modal-target="popup-modal" data-modal-toggle="popup-modal"
-        class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        class="block text-white font-bold rounded-lg text-sm px-5 py-2.5 text-center bg-red-600 hover:bg-red-800 hover:text-[53,53,53] transition ease-in-out duration-100"
         type="button">
 
         Logout
@@ -13,7 +13,7 @@
         <div class="relative w-full max-w-md max-h-full p-4">
             <div class="relative bg-white rounded-lg shadow ">
                 <button type="button"
-                    class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center "
+                    class="absolute top-3 end-2.5 hover:bg-[rgb(52,52,52)] transition duration-100 ease-in-out hover:text-gray-100 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center "
                     data-modal-hide="popup-modal">
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                         viewBox="0 0 14 14">
@@ -23,20 +23,31 @@
                     <span class="sr-only">Close modal</span>
                 </button>
                 <div class="p-4 text-center md:p-5">
-                    <svg class="w-12 h-12 mx-auto mb-4 text-gray-400 " aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                    </svg>
-                    <h3 class="mb-5 text-lg font-normal text-gray-500 ">Are you sure you want to
-                        Logout?</h3>
-                    <button data-modal-hide="popup-modal" type="button" wire:click="logout"
-                        class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
-                        Yes, I'm sure
-                    </button>
-                    <button data-modal-hide="popup-modal" type="button"
-                        class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 ">No,
-                        cancel</button>
+                    <div class="flex flex-row items-center justify-center gap-2 mb-4">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="text-red-600 size-12">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+                            </svg>
+                        </div>
+                        <div>
+                            <p class="text-2xl font-bold">Logout Confirmation</p>
+                        </div>
+                    </div>
+                    <div>
+                        <h3 class="mb-8 text-md font-normal text-[rgb(53,53,53)] ">Are you sure you want to
+                            Logout?</h3>
+                    </div>
+                    <div class="flex flex-row items-center justify-center gap-2">
+                        <button data-modal-hide="popup-modal" type="button"
+                            class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 transition duration-100 ease-in-out">No,
+                            cancel</button>
+                        <button data-modal-hide="popup-modal" type="button" wire:click="logout"
+                            class="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center transition duration-100 ease-in-out">
+                            Yes, I'm sure
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
