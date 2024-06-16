374425997a90a0da7207876ce0eac9a0a997a562
darealZoid <darealZoid@gmail.com>
2024-06-16 23:54:29 +0800

    added sort for company name

--- resources/views/livewire/components/SupplierManagement/supplier-table.blade.php
+++ resources/views/livewire/components/SupplierManagement/supplier-table.blade.php
@@ -69,7 +69,23 @@
                     <tr class=" text-nowrap">
 
                         {{-- //* company name --}}
-                        <th scope="col" class="px-4 py-3">Company Name</th>
+                        <th wire:click="sortByColumn('updated_at')" scope="col"
+                            class=" text-nowrap gap-2 px-4 py-3 transition-all duration-100 ease-in-out cursor-pointer hover:bg-[#464646] hover:text-white">
+
+                            <div class="flex items-center">
+
+                                <p>Company Name</p>
+
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                                    </svg>
+                                </span>
+
+                            </div>
+                        </th>
 
                         {{-- //* contact number --}}
                         <th scope="col" class="px-4 py-3">Contact No</th>
@@ -111,7 +127,7 @@
 
                             <div class="flex items-center">
 
-                                <p>Updated at at</p>
+                                <p>Updated at</p>
 
                                 <span>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -135,7 +151,7 @@
                 {{-- //* table body --}}
                 <tbody>
 
-                    
+
 
 
                 </tbody>
