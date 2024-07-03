816bc01140f69016c89303af4e2a3107c0cbbe9e
Aceboy76 <harthpalaras62@gmail.com>
2024-07-03 13:14:03 +0800

    fix chart to auto reload

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -24,6 +24,8 @@
 
     @vite('resources/js/app.js')
     @livewireScripts()
+
+
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <x-livewire-alert::scripts />
--- resources/views/livewire/charts/user-chart.blade.php
+++ resources/views/livewire/charts/user-chart.blade.php
@@ -3,58 +3,61 @@
             <canvas width="400" height="100" id="myChart"></canvas>
         </div>
 
-        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        @assets
+            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        @endassets
 
-        <script>
-            const ctx = document.getElementById('myChart');
+        @script
+            <script>
+                const ctx = document.getElementById('myChart');
 
 
-            const data = {
-                labels: ['Active', 'Inactive'],
-                datasets: [{
-                    data: [{{ $activeUsersCount }}, {{ $inactiveUsersCount }}],
-                    backgroundColor: [
-                        'rgba(255, 99, 132)',
-                        'rgba(255, 159, 64`)',
-                    ],
-                    borderColor: [
-                        'rgb(255, 99, 132)',
-                        'rgb(255, 159, 64)',
-                    ],
-                    borderWidth: 1
-                }]
-            };
+                const data = {
+                    labels: ['Active', 'Inactive'],
+                    datasets: [{
+                        data: [{{ $activeUsersCount }}, {{ $inactiveUsersCount }}],
+                        backgroundColor: [
+                            'rgba(255, 99, 132)',
+                            'rgba(255, 159, 64`)',
+                        ],
+                        borderColor: [
+                            'rgb(255, 99, 132)',
+                            'rgb(255, 159, 64)',
+                        ],
+                        borderWidth: 1
+                    }]
+                };
 
-            new Chart(ctx, {
-                type: 'bar',
-                data: data,
-                options: {
-                    plugins: {
-                        legend: {
-                            display: false
+                new Chart(ctx, {
+                    type: 'bar',
+                    data: data,
+                    options: {
+                        plugins: {
+                            legend: {
+                                display: false
+                            },
+                            title: {
+                                display: true,
+                                text: 'Users'
+                            }
                         },
-                        title: {
-                            display: true,
-                            text: 'Users'
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            },
+                            xAxes: [{
+                                gridLines: {
+                                    display: false
+                                }
+                            }],
+                            yAxes: [{
+                                gridLines: {
+                                    display: false
+                                }
+                            }]
                         }
-                    },
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        },
-                        xAxes: [{
-                            gridLines: {
-                                display: false
-                            }
-                        }],
-                        yAxes: [{
-                            gridLines: {
-                                display: false
-                            }
-                        }]
                     }
-                }
-            });
-        </script>
-
+                });
+            </script>
+        @endscript
     </div>
