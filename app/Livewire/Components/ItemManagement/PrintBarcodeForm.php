aecc82b02a1cb585d4aa134262d74019d206d785
Aceboy76 <harthpalaras62@gmail.com>
2024-07-12 15:36:15 +0800

    added print barcode form

---
+++ app/Livewire/Components/ItemManagement/PrintBarcodeForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Components\ItemManagement;
+
+use Livewire\Component;
+
+class PrintBarcodeForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.ItemManagement.print-barcode-form');
+    }
+}
