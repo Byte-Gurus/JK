16169699a2b3d65114a092ae0ed994c0cff50459
darealZoid <darealZoid@gmail.com>
2024-07-13 22:18:11 +0800

    profile icon

--- resources/views/livewire/components/UserManagement/user-form.blade.php
+++ resources/views/livewire/components/UserManagement/user-form.blade.php
@@ -190,44 +190,32 @@
                                 </div>
 
                                 {{-- fourth row --}}
-
-                                <div class="grid justify-between grid-flow-col grid-cols-2 gap-4">
-                                    <div class="mb-3">
-                                        <label for="profile"
-                                            class="block mb-2 text-sm font-medium text-gray-900 ">Profile</label>
-
-
-                                        {{-- default profile --}}
-                                        @if ($user_image)
-                                            <img src="{{ $user_image->temporaryUrl() }}" alt="">
-                                        @else
-                                        
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                                                fill="gray"
-                                                class="rounded-full w-15 h-15 border-spacing-0 border-black border-2">
-                                                <path fillRule="evenodd"
-                                                    d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-                                                    clipRule="evenodd" />
-                                            </svg>
-                                        @endif
-
-
-
-                                    </div>
-
-
-                                    <div class="mb-3">
-
-                                        <input
-                                            class="block round text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer w-content bg-gray-50 focus:outline-none"
-                                            aria-describedby="user_avatar_help" id="user_image"
-                                            wire:model="user_image" type="file">
-
-                                        @error('user_image')
-                                            <span class="font-medium text-red-500 error">{{ $message }}</span>
-                                        @enderror
-
-
+                                <div class="mb-3">
+                                    <label for="profile"
+                                        class="block mb-2 text-sm font-medium text-gray-900 ">Profile</label>
+                                    <div class="grid justify-between grid-flow-col grid-cols-2 gap-4">
+                                        <div class="flex items-center justify-center mb-3">
+                                            {{-- default profile --}}
+                                            @if ($user_image)
+                                                <img class="border-4 border-black rounded-full p-auto" src="{{ $user_image->temporaryUrl() }}" alt="">
+                                            @else
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                    class="p-4 border-4 border-black rounded-full size-36">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                                                </svg>
+                                            @endif
+                                        </div>
+                                        <div class="flex items-center justify-center mb-3">
+                                            <input
+                                                class="block text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer round w-content bg-gray-50 focus:outline-none"
+                                                aria-describedby="user_avatar_help" id="user_image"
+                                                wire:model="user_image" type="file">
+                                            @error('user_image')
+                                                <span class="font-medium text-red-500 error">{{ $message }}</span>
+                                            @enderror
+                                        </div>
                                     </div>
                                 </div>
                             </div>
