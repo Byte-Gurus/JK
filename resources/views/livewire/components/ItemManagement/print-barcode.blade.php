24bb5c61d889c7130dcbfde6bf76878561fab7f8
darealZoid <darealZoid@gmail.com>
2024-07-16 14:56:02 +0800

    fixed positioning

--- resources/views/livewire/components/ItemManagement/print-barcode.blade.php
+++ resources/views/livewire/components/ItemManagement/print-barcode.blade.php
@@ -1,18 +1,13 @@
-<div class="w-screen h-screen fixed inset-0 z-40 bg-white flex-wrap " x-show="isPrint">
-
+<div class="fixed inset-0 z-40 flex flex-row flex-wrap items-center justify-center w-screen h-screen gap-2 overflow-y-scroll bg-white"
+    x-show="isPrint">
     @if ($barcode)
         @for ($i = 0; $i < $barcode_quantity; $i++)
             <div class="m-5 ">
-
                 <img id="barcode" wire:model="barcode">{!! DNS1D::getBarcodeSVG($barcode, 'C128', 1, 60) !!}</img>
-
-
                 @error('barcode')
                     <span class="font-medium text-red-500 error">{{ $message }}</span>
                 @enderror
             </div>
         @endfor
-
     @endif
-
 </div>
