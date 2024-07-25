e1a1960753f9a2d3a8b472ef04088375ac48415e
Aceboy76 <harthpalaras62@gmail.com>
2024-07-25 19:49:55 +0800

    update

--- database/factories/AddressFactory.php
+++ database/factories/AddressFactory.php
@@ -20,18 +20,29 @@ class AddressFactory extends Factory
     public function definition(): array
     {
 
-        $province = PhilippineProvince::inRandomOrder()->first();
+       // Randomly pick a province
+       $province = PhilippineProvince::inRandomOrder()->first();
 
-        $city = PhilippineCity::where('province_code', $province->province_code)->inRandomOrder()->first();
+       // If a province is found, get a city or municipality from that province
+       if ($province) {
+           $city = PhilippineCity::where('province_code', $province->province_code)->inRandomOrder()->first();
 
-        $barangay = PhilippineBarangay::where('city_municipality_code', $city->city_municipality_code)->inRandomOrder()->first();
+           // If a city or municipality is found, get a barangay from that city
+           if ($city) {
+               $barangay = PhilippineBarangay::where('city_municipality_code', $city->city_municipality_code)->inRandomOrder()->first();
+           } else {
+               $barangay = null; // Handle case when no city is found
+           }
+       } else {
+           $city = null; // Handle case when no province is found
+           $barangay = null;
+       }
 
-        return [
-            
-            'province_code' => $province->province_code,
-            'city_municipality_code' => $city->city_municipality_code,
-            'barangay_code' => $barangay->barangay_code,
-            'street' => fake()->streetName(),
-        ];
+       return [
+           'province_code' => $province->province_code ?? null,
+           'city_municipality_code' => $city->city_municipality_code ?? null,
+           'barangay_code' => $barangay->barangay_code ?? null,
+           'street' => $this->faker->streetName(),
+       ];
     }
 }
