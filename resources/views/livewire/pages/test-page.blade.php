125c67a6b09c45395b4f770e49f0eeea3011baf1
Aceboy76 <harthpalaras62@gmail.com>
2024-07-26 13:11:57 +0800

    update

--- resources/views/livewire/pages/test-page.blade.php
+++ resources/views/livewire/pages/test-page.blade.php
@@ -1,12 +1,14 @@
 <div>
-    <form wire:submit="save">
+    <form wire:submit="save" action="" enctype="multipart/form-data">
         @if ($photo)
             <img src="{{ $photo->temporaryUrl() }}">
         @endif
 
         <input type="file" wire:model="photo">
 
-        @error('photo') <span class="error">{{ $message }}</span> @enderror
+        @error('photo')
+            <span class="error">{{ $message }}</span>
+        @enderror
 
         <button type="submit">Save photo</button>
         sss
