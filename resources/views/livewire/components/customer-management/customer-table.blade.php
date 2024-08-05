3230e94428ea84acd79c91a217b69a875254c7b5
darealZoid <darealZoid@gmail.com>
2024-08-05 19:05:39 +0800

    z

--- resources/views/livewire/components/customer-management/customer-table.blade.php
+++ resources/views/livewire/components/customer-management/customer-table.blade.php
@@ -11,11 +11,10 @@
             <div class="relative w-full">
 
                 <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-black" fill="none" viewBox="0 0 24 24"
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-black " fill="none" viewBox="0 0 24 24"
                         strokeWidth={1.5} stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round"
-                            d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                     </svg>
                 </div>
                 <input type="text" wire:model.live.debounce.100ms="search"
