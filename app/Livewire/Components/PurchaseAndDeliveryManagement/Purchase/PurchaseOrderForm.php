3beed241278456891428b8d9663d7a959c26dd19
Aceboy76 <harthpalaras62@gmail.com>
2024-08-07 17:08:46 +0800

    update

--- app/Livewire/Components/PurchaseAndDeliveryManagement/Purchase/PurchaseOrderForm.php
+++ app/Livewire/Components/PurchaseAndDeliveryManagement/Purchase/PurchaseOrderForm.php
@@ -24,34 +24,37 @@ class PurchaseOrderForm extends Component
         $suppliers = Supplier::select('id', 'company_name')->get();
 
         $reorder_lists = Item::leftJoin('inventories', 'items.id', '=', 'inventories.item_id')
-            ->select(
-                'items.id as item_id',
-                'items.barcode',
-                'items.item_name',
-                'items.item_description',
-                'items.maximum_stock_ratio',
-                'items.reorder_percentage',
-                'items.reorder_point',
-                'items.vat_amount',
-                'items.vat_type',
-                'items.status_id',
-                DB::raw('COALESCE(SUM(inventories.quantity), 0) as total_quantity'),
-                DB::raw('MAX(inventories.status) as inventory_status')
-            )
-            ->groupBy(
-                'items.id',
-                'items.barcode',
-                'items.item_name',
-                'items.item_description',
-                'items.maximum_stock_ratio',
-                'items.reorder_percentage',
-                'items.reorder_point',
-                'items.vat_amount',
-                'items.vat_type',
-                'items.status_id'
-            )
-            ->havingRaw('total_quantity = 0 OR inventory_status = "Available"')
-            ->get();
+    ->select(
+        'items.id as item_id',
+        'items.barcode',
+        'items.item_name',
+        'items.item_description',
+        'items.maximum_stock_ratio',
+        'items.reorder_percentage',
+        'items.reorder_point',
+        'items.vat_amount',
+        'items.vat_type',
+        'items.status_id',
+        DB::raw('COALESCE(SUM(inventories.quantity), 0) as total_quantity'),
+        DB::raw('MAX(inventories.status) as inventory_status')
+    )
+    ->where('items.status_id', '<>', '2') // Replace 'inactive_status_id' with the actual value representing inactive status
+    ->groupBy(
+        'items.id',
+        'items.barcode',
+        'items.item_name',
+        'items.item_description',
+        'items.maximum_stock_ratio',
+        'items.reorder_percentage',
+        'items.reorder_point',
+        'items.vat_amount',
+        'items.vat_type',
+        'items.status_id'
+    )
+    ->havingRaw('total_quantity = 0 OR inventory_status = "Available"')
+    ->get();
+
+
         $this->generatePurchaseNumber();
 
         return view('livewire.components.PurchaseAndDeliveryManagement.Purchase.purchase-order-form', compact('suppliers', 'reorder_lists'));
