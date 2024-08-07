3198044778fd1c6f8711655071ddd79017e5c255
darealZoid <darealZoid@gmail.com>
2024-08-07 15:02:12 +0800

    Revert "stock adjust form"
    This reverts commit 86c6e2c5f6373a26c11626646dd1b0cdf2d4ad30.

--- app/Livewire/Pages/InventoryManagementPage.php
+++ app/Livewire/Pages/InventoryManagementPage.php
@@ -7,7 +7,7 @@ use Livewire\Component;
 
 class InventoryManagementPage extends Component
 {
-    public $showStockAdjustModal = false;
+    public $showModal = false;
     public $sidebarStatus;
     public function render()
     {
@@ -21,7 +21,7 @@ class InventoryManagementPage extends Component
 
     public function closeModal()
     {
-        $this->showStockAdjustModal = false;
+        $this->showModal = false;
     }
 
     public function formCreate()
