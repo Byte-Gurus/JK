08fd34029ec1dda532e52644c2f4b6b4fddcb287
darealZoid <darealZoid@gmail.com>
2024-08-16 21:05:34 +0800

    aiah

--- app/Livewire/Components/InventoryManagement/StockAdjustForm.php
+++ app/Livewire/Components/InventoryManagement/StockAdjustForm.php
@@ -12,4 +12,15 @@ class StockAdjustForm extends Component
     {
         return view('livewire.components.InventoryManagement.stock-adjust-form');
     }
+
+    private function resetForm() //*tanggalin ang laman ng input pati $user_id value
+    {
+        // $this->reset(['firstname', 'middlename', 'lastname', 'contact_number', 'role', 'status', 'username', 'password', 'retype_password', 'user_id']);
+    }
+
+    public function resetFormWhenClosed()
+    {
+        $this->resetForm();
+        // $this->resetValidation();
+    }
 }
