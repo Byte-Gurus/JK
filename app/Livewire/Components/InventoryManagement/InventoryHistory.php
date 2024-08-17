89f5d2baec3d4b73e39fe0aecea119ad5f9d1d89
darealZoid <darealZoid@gmail.com>
2024-08-17 23:24:11 +0800

    inventory history

---
+++ app/Livewire/Components/InventoryManagement/InventoryHistory.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Components\InventoryManagement;
+
+use Livewire\Component;
+
+class InventoryHistory extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.InventoryManagement.inventory-history');
+    }
+}
--- app/Livewire/Pages/InventoryManagementPage.php
+++ app/Livewire/Pages/InventoryManagementPage.php
@@ -10,6 +10,9 @@ class InventoryManagementPage extends Component
     public $showModal = false;
 
     public $showStockAdjustModal = false;
+
+    public $showInventoryHistory = false;
+
     public $sidebarStatus;
     public function render()
     {
@@ -30,6 +33,11 @@ class InventoryManagementPage extends Component
     public function formCreate()
     {
         $this->dispatch('change-method', isCreate: true)->to(InventoryForm::class);
+    }
+
+    public function showInventory()
+    {
+        $this->showInventoryHistory = !$this->showInventoryHistory;
     }
 
     public function changeSidebarStatus($sidebarOpen)
