2b66a1f2db51554cf2f0f3af484b4e62154fa935
darealZoid <darealZoid@gmail.com>
2024-08-19 12:57:37 +0800

    restock form

---
+++ app/Livewire/Components/PurchaseAndDeliveryManagement/Delivery/RestockForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Livewire\Components\PurchaseAndDeliveryManagement\Delivery;
+
+use App\Livewire\Pages\DeliveryPage;
+use Livewire\Component;
+
+class RestockForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.PurchaseAndDeliveryManagement.Delivery.restock-form');
+    }
+
+
+}
