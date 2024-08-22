a90f62eda9e887aeb26da6fd7a3b3e0ea429d17d
darealZoid <darealZoid@gmail.com>
2024-08-22 14:52:43 +0800

    z

--- resources/views/livewire/components/UserManagement/user-table.blade.php
+++ resources/views/livewire/components/UserManagement/user-table.blade.php
@@ -177,31 +177,31 @@
 
 
                             {{-- //* name --}}
-                            <th scope="row" class="px-4 py-6 font-medium text-gray-900 text-md whitespace-nowrap ">
+                            <th scope="row" class="px-4 py-4 font-medium text-gray-900 text-md whitespace-nowrap ">
                                 {{ $user->firstname . ' ' . $user->middlename . ' ' . $user->lastname }}
                             </th>
 
                             {{-- //* contact number --}}
-                            <th scope="row" class="px-4 py-6 font-medium text-gray-900 text-md whitespace-nowrap ">
+                            <th scope="row" class="px-4 py-4 font-medium text-gray-900 text-md whitespace-nowrap ">
                                 {{ $user->contact_number }}
                             </th>
 
                             {{-- //* role --}}
-                            <th scope="row" class="px-4 py-6 font-medium text-gray-900 text-md whitespace-nowrap ">
+                            <th scope="row" class="px-4 py-4 font-medium text-gray-900 text-md whitespace-nowrap ">
                                 {{ $user->roleJoin->role }}
                             </th>
 
                             {{-- //* status --}}
                             <th scope="row"
-                                class="px-4 py-6 font-medium text-center pointer-events-none text-md whitespace-nowrap">
+                                class="px-4 py-4 font-medium text-center pointer-events-none text-md whitespace-nowrap">
 
                                 {{-- //* active green, if inactive red --}}
                                 <p
-                                    @if ($user->statusJoin->status_type == 'Active') class=" text-black  bg-green-400 border border-green-900   text-xs text-center font-medium px-2 py-0.5 rounded"
+                                    @if ($user->statusJoin->status_type == 'Active') class=" text-green-900 pointer-events-none  bg-green-100 border border-green-900 text-xs text-center font-medium px-2 py-0.5 rounded-sm"
 
                                         @elseif ($user->statusJoin->status_type == 'Inactive')
 
-                                        class=" text-black bg-rose-400 border border-red-900 text-xs font-medium px-2 py-0.5 rounded " @endif>
+                                        class=" text-red-900 bg-red-100 border pointer-events-none border-red-900 text-xs font-medium px-2 py-0.5 rounded-sm " @endif>
 
                                     {{ $user->statusJoin->status_type }}
                                 </p>
@@ -209,22 +209,22 @@
                             </th>
 
                             {{-- //* username --}}
-                            <th scope="row" class="px-4 py-6 font-medium text-gray-900 text-md whitespace-nowrap ">
+                            <th scope="row" class="px-4 py-4 font-medium text-gray-900 text-md whitespace-nowrap ">
                                 {{ $user->username }}
                             </th>
 
                             {{-- //* created at --}}
-                            <th scope="row" class="px-4 py-6 font-medium text-gray-900 text-md whitespace-nowrap ">
+                            <th scope="row" class="px-4 py-4 font-medium text-gray-900 text-md whitespace-nowrap ">
                                 {{ $user->created_at->format('d-m-y h:i A') }}
                             </th>
 
                             {{-- //* updated at --}}
-                            <th scope="row" class="px-4 py-6 font-medium text-gray-900 text-md whitespace-nowrap ">
+                            <th scope="row" class="px-4 py-4 font-medium text-gray-900 text-md whitespace-nowrap ">
                                 {{ $user->updated_at->format('d-m-y h:i A') }}
                             </th>
 
                             {{-- //* edit button --}}
-                            <th class="px-4 py-6 text-center text-md text-nowrap">
+                            <th class="px-4 py-4 text-center text-md text-nowrap">
                                 <div
                                     class="flex items-center justify-center px-1 py-1 font-medium text-blue-600 rounded-sm hover:bg-blue-100 ">
 
