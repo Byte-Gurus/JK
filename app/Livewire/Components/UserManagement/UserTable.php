dfdb388f5aeb0aae12ab9332f9a7a36f9824ae6a
Aceboy76 <harthpalaras62@gmail.com>
2024-09-12 20:44:43 +0800

    ass

--- app/Livewire/Components/UserManagement/UserTable.php
+++ app/Livewire/Components/UserManagement/UserTable.php
@@ -14,8 +14,8 @@ class UserTable extends Component
 {
     use WithPagination,  WithoutUrlPagination;
 
-    public $sortDirection = 'desc';//var default sort direction is ascending
-    public $sortColumn = 'id';//var defualt sort is ID
+    public $sortDirection = 'desc'; //var default sort direction is ascending
+    public $sortColumn = 'id'; //var defualt sort is ID
     public $perPage = 10; //var for pagination
     public $search = '';  //var search component
     public $roleFilter = 0; //var filtering value = all
@@ -38,8 +38,8 @@ class UserTable extends Component
         //*if ang roleFilter is 0 walang filter ang maapply
 
         $users = $query->search($this->search) //?search the user
-        ->orderBy($this->sortColumn, $this->sortDirection) //? i sort ang column based sa $sortColumn na var
-        ->paginate($this->perPage);  //?  and paginate it
+            ->orderBy($this->sortColumn, $this->sortDirection) //? i sort ang column based sa $sortColumn na var
+            ->paginate($this->perPage);  //?  and paginate it
 
         $roles = UserRole::all(); //? kunin lahat ng role
 
@@ -48,7 +48,7 @@ class UserTable extends Component
 
 
     protected $listeners = [
-        'refresh-table' => 'refreshTable',//*  galing sa UserTable class
+        'refresh-table' => 'refreshTable', //*  galing sa UserTable class
         // "echo:new-user,NewUserCreatedEvent" => 'updatedSearch',
         "echo:refresh-user,UserEvent" => 'refreshFromPusher',
 
@@ -62,8 +62,8 @@ class UserTable extends Component
         //@params userID name ng parameter na ipapasa, $userId parameter value na ipapasa
         $this->dispatch('edit-user-from-table', userID: $userId)->to(UserForm::class);
 
-         //*call the listesner 'change-method' galing sa UserForm class
-         //@params isCerate name ng parameter na ipapasa, false parameter value na ipapasa, false kasi d ka naman mag create user
+        //*call the listesner 'change-method' galing sa UserForm class
+        //@params isCerate name ng parameter na ipapasa, false parameter value na ipapasa, false kasi d ka naman mag create user
         $this->dispatch('change-method', isCreate: false)->to(UserForm::class);
     }
 
@@ -76,20 +76,22 @@ class UserTable extends Component
     }
 
 
-    public function sortByColumn($column){ //* sort the column
+    public function sortByColumn($column)
+    { //* sort the column
 
         //* if ang $column is same sa global variable na sortColumn then if ang sortDirection is desc then it will be asc
-        if($this->sortColumn = $column){
-            $this->sortDirection = $this->sortDirection == 'asc'?'desc':'asc';
-        } else{
+        if ($this->sortColumn = $column) {
+            $this->sortDirection = $this->sortDirection == 'asc' ? 'desc' : 'asc';
+        } else {
             //* if hindi same ang $column sa global variable na sortColumn, then gawing asc ang column
             $this->sortDirection = 'asc';
         }
 
-       $this->sortColumn = $column; //* gawing global variable ang $column
+        $this->sortColumn = $column; //* gawing global variable ang $column
     }
     public function refreshFromPusher()
     {
+        dd("sasa");
         $this->resetPage();
     }
 
