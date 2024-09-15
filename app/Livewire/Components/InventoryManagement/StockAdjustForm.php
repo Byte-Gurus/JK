b6bbc642846ccd9c2456e5e5ac7cdde523dc1e62
Aceboy76 <harthpalaras62@gmail.com>
2024-09-15 13:58:43 +0800

    removed 2 admin login

--- app/Livewire/Components/InventoryManagement/StockAdjustForm.php
+++ app/Livewire/Components/InventoryManagement/StockAdjustForm.php
@@ -179,6 +179,7 @@ class StockAdjustForm extends Component
     public function adminConfirmed($isAdmin){
         $this->isAdmin = $isAdmin;
 
+        dd($this->isAdmin);
         if($this->isAdmin){
             $this->displayStockAdjustConfirmation();
         }
