f04f31cc73ecbacef528cf954eea22ec02765b12
Aceboy76 <harthpalaras62@gmail.com>
2024-09-17 23:03:18 +0800

    ass

--- app/Livewire/Components/Sales/SalesTransaction.php
+++ app/Livewire/Components/Sales/SalesTransaction.php
@@ -824,6 +824,7 @@ class SalesTransaction extends Component
 
                 return $reorderPeriods;
             })->min();
+            $minReorderPeriod = $minReorderPeriod !== null ? (int) $minReorderPeriod : 0;
             dd($minReorderPeriod);
             // dd($minReorderPeriod);
 
