798f9b771155528b7cbb4186c4515f31c37d6831
Aceboy76 <harthpalaras62@gmail.com>
2024-09-18 13:21:05 +0800

    ass

--- app/Livewire/Components/ReportManagement/BackorderedItemsReport.php
+++ app/Livewire/Components/ReportManagement/BackorderedItemsReport.php
@@ -2,12 +2,26 @@
 
 namespace App\Livewire\Components\ReportManagement;
 
+use App\Models\BackOrder;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class BackorderedItemsReport extends Component
 {
+    public $createdBy, $dateCreated;
     public function render()
     {
-        return view('livewire.components.ReportManagement.backordered-items-report');
+        $backorderLists = BackOrder::where('status', 'Missing')->get();
+        
+        return view('livewire.components.ReportManagement.backordered-items-report', [
+            'backorderLists' => $backorderLists
+        ]);
+    }
+    public function reportInfo()
+    {
+        $this->createdBy = Auth::user()->firstname . ' ' . (Auth::user()->middlename ? Auth::user()->middlename . ' ' : '') . Auth::user()->lastname;
+
+        $this->dateCreated = Carbon::now()->format('m d Y h:i:s a');
     }
 }
