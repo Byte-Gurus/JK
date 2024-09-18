bd1468a75bd0779584760c872e56d7a1b07b4919
Aceboy76 <harthpalaras62@gmail.com>
2024-09-18 13:45:50 +0800

    ass

--- app/Livewire/Components/ReportManagement/ExpiredItemsReport.php
+++ app/Livewire/Components/ReportManagement/ExpiredItemsReport.php
@@ -2,12 +2,27 @@
 
 namespace App\Livewire\Components\ReportManagement;
 
+use App\Models\Inventory;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class ExpiredItemsReport extends Component
 {
+    public $createdBy, $dateCreated;
+
     public function render()
     {
-        return view('livewire.components.ReportManagement.expired-items-report');
+        $expiredItems = Inventory::where('status', 'Expired')->get();
+        return view('livewire.components.ReportManagement.expired-items-report',[
+            'expiredItems' => $expiredItems
+        ]);
+    }
+
+    public function reportInfo()
+    {
+        $this->createdBy = Auth::user()->firstname . ' ' . (Auth::user()->middlename ? Auth::user()->middlename . ' ' : '') . Auth::user()->lastname;
+
+        $this->dateCreated = Carbon::now()->format('m d Y h:i:s a');
     }
 }
--- resources/views/livewire/components/ReportManagement/expired-items-report.blade.php
+++ resources/views/livewire/components/ReportManagement/expired-items-report.blade.php
@@ -16,100 +16,91 @@
                 <div class="flex flex-col ">
                     <div class="flex flex-row text-nowrap">
                         <p class="text-[1em] font-bold uppercase">Date & Time Created:</p>
-                        {{-- <p class="text-[1em] font-bold uppercase">{{ $dateCreated }}</p> --}}
-                        {{-- {{ $receiptDetails['transaction_info']['transaction_date'] ?? null }} --}}
+                        <p class="text-[1em] font-bold uppercase">{{ $dateCreated }}</p>
                         </p>
                     </div>
                     <div class="flex flex-row text-nowrap">
                         <p class="text-[1em] font-bold uppercase">Prepared By:</p>
-                        {{-- <p class="text-[1em] font-bold uppercase">{{ $createdBy }}</p> --}}
-                        {{-- {{ $receiptDetails['payment']['payment_type'] ?? null }}</p> --}}
+                        <p class="text-[1em] font-bold uppercase">{{ $createdBy }}</p>
                     </div>
                 </div>
             </div>
             <div>
-                <p class="text-[1.4em] font-bold text-right italic m-4 mr-10 uppercase">EXPIRED ITEMS LIST REPORT</p>
+                <p class="text-[1.4em] font-bold text-right italic m-4 mr-10 uppercase">STOCK-ON-HAND REPORT</p>
             </div>
         </div>
 
         <div>
             <div class="w-full my-4 border-b border-black"> </div>
 
-            {{-- //* table header --}}
-            <ul class="grid justify-between grid-flow-col grid-cols-5 mx-4 ">
+
+
+
+            <ul class="grid justify-between grid-flow-col grid-cols-3 mx-4 ">
 
                 <li class="col-span-1 ">
                     <div>
-                        <p class="text-[0.8em] uppercase text-left font-bold">Barcode</p>
-                    </div>
-                </li>
-
-                <li class="col-span-1 ">
-                    <div>
-                        <p class="text-[0.8em] uppercase text-left font-bold">SKU</p>
-                    </div>
-                </li>
-
-                <li class="col-span-1 ">
-                    <div>
-                        <p class="text-[0.8em] uppercase text-left font-bold">Item Description</p>
+                        <p class="text-[1em] uppercase text-left font-bold">Barcode</p>
                     </div>
                 </li>
                 <li class="col-span-1 ">
                     <div>
-                        <p class="text-[0.8em] uppercase text-center font-bold">Item Quantity</p>
+                        <p class="text-[1em] uppercase text-center font-bold">Item Name</p>
                     </div>
                 </li>
-
                 <li class="col-span-1 ">
                     <div>
-                        <p class="text-[0.8em] uppercase text-center font-bold">Date</p>
+                        <p class="text-[1em] uppercase text-center font-bold">Item Description</p>
                     </div>
                 </li>
-
+                <li class="col-span-1 ">
+                    <div>
+                        <p class="text-[1em] uppercase text-center font-bold">Item Quantity</p>
+                    </div>
+                </li>
             </ul>
 
             <div class="w-full my-4 border-b border-black"> </div>
 
-            {{-- @foreach ($purchaseDetails as $purchaseDetail) --}}
-            <ul class="grid justify-between grid-flow-col grid-cols-5 mx-4 ">
+            @foreach ($expiredItems as $expiredItem)
+                <ul class="grid justify-between grid-flow-col grid-cols-3 mx-4 ">
 
-                <li class="col-span-1 py-[3px]">
-                    <div>
-                        <p class="text-[0.8em] text-left font-medium">
-                            hiii</p>
-                    </div>
-                </li>
-                <li class="col-span-1 py-[3px]">
-                    <div>
-                        <p class="text-[0.8em] text-left font-bold">
-                            hello
-                        </p>
-                    </div>
-                </li>
-                <li class="col-span-1 py-[3px]">
-                    <div>
-                        <p class="text-[0.8em] text-center fot-bold">
-                            aiah
-                        </p>
-                    </div>
-                </li>
-                <li class="col-span-1 py-[3px]">
-                    <div>
-                        <p class="text-[0.8em] text-center fot-bold">
-                            aiah
-                        </p>
-                    </div>
-                </li>
-                <li class="col-span-1 py-[3px]">
-                    <div>
-                        <p class="text-[0.8em] text-center fot-bold">
-                            aiah
-                        </p>
-                    </div>
-                </li>
-            </ul>
-            {{-- @endforeach --}}
+                    <li class="col-span-1 py-[3px]">
+                        <div>
+                            <p class="text-[1em] text-left font-medium">
+                                {{ $expiredItem->itemJoin->barcode }}</p>
+                        </div>
+                    </li>
+                    <li class="col-span-1 text-center py-[3px]">
+                        <div>
+                            <p class="text-[1em] text-center font-bold">
+                                {{ $expiredItem->itemJoin->item_name }}
+                            </p>
+                        </div>
+                    </li>
+                    <li class="col-span-1 text-center py-[3px]">
+                        <div>
+                            <p class="text-[1em] text-center font-bold">
+                                {{ $expiredItem->itemJoin->item_description }}
+                            </p>
+                        </div>
+                    </li>
+                    <li class="col-span-1 py-[3px]">
+                        <div>
+                            <p class="text-[1em] text-center fot-bold">
+                                {{ $expiredItem->current_stock_quantity }}
+                            </p>
+                        </div>
+                    </li>
+                    <li class="col-span-1 py-[3px]">
+                        <div>
+                            <p class="text-[1em] text-center fot-bold">
+                                {{ $expiredItem->expiration_date }}
+                            </p>
+                        </div>
+                    </li>
+                </ul>
+            @endforeach
         </div>
     </div>
 </div>
