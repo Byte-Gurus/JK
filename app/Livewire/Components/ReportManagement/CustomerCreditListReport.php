44cec15f12d54491df4a73563133ef95d479071b
Aceboy76 <harthpalaras62@gmail.com>
2024-09-20 00:00:42 +0800

    assa

--- app/Livewire/Components/ReportManagement/CustomerCreditListReport.php
+++ app/Livewire/Components/ReportManagement/CustomerCreditListReport.php
@@ -2,12 +2,16 @@
 
 namespace App\Livewire\Components\ReportManagement;
 
+use App\Models\Credit;
 use Livewire\Component;
 
 class CustomerCreditListReport extends Component
 {
     public function render()
     {
-        return view('livewire.components.ReportManagement.customer-credit-list-report');
+        credit_limit
+        return view('livewire.components.ReportManagement.customer-credit-list-report',[
+            'credits' => $credits
+        ]);
     }
 }
--- resources/views/livewire/components/ReportManagement/customer-credit-list-report.blade.php
+++ resources/views/livewire/components/ReportManagement/customer-credit-list-report.blade.php
@@ -22,21 +22,21 @@
 
         </div>
         {{-- @if ($transaction_info) --}}
-            <div class="flex flex-col justify-between col-span-1 px-4 mb-2">
-                <div class="flex flex-col ">
-                    <div class="flex flex-row text-nowrap">
-                        <p class="text-[1em] font-bold uppercase">Date & Time Created:</p>
-                        {{-- {{ $transaction_info['dateCreated'] ?? ' ' }} --}}
+        <div class="flex flex-col justify-between col-span-1 px-4 mb-2">
+            <div class="flex flex-col ">
+                <div class="flex flex-row text-nowrap">
+                    <p class="text-[1em] font-bold uppercase">Date & Time Created:</p>
+                    {{-- {{ $transaction_info['dateCreated'] ?? ' ' }} --}}
 
-                        </p>
-                    </div>
-                    <div class="flex flex-row text-nowrap">
-                        <p class="text-[1em] font-bold uppercase">Prepared By:</p>
-                        {{-- {{ $transaction_info['createdBy'] ?? ' ' }} --}}
+                    </p>
+                </div>
+                <div class="flex flex-row text-nowrap">
+                    <p class="text-[1em] font-bold uppercase">Prepared By:</p>
+                    {{-- {{ $transaction_info['createdBy'] ?? ' ' }} --}}
 
-                    </div>
                 </div>
             </div>
+        </div>
         {{-- @endif --}}
 
         <div>
@@ -58,11 +58,6 @@
                 <li class="col-span-1 ">
                     <div>
                         <p class="text-[0.8em] uppercase text-left font-bold">Contact No.</p>
-                    </div>
-                </li>
-                <li class="col-span-1 ">
-                    <div>
-                        <p class="text-[0.8em] uppercase text-center font-bold">Credit ID</p>
                     </div>
                 </li>
 
@@ -88,60 +83,54 @@
 
             <div class="w-full my-4 border-b border-black"> </div>
 
-            {{-- @foreach ($transactions as $transaction) --}}
-            <ul class="grid justify-between grid-flow-col grid-cols-7 mx-4 ">
+            @foreach ($credits as $credit)
+                <ul class="grid justify-between grid-flow-col grid-cols-7 mx-4 ">
 
-                <li class="col-span-1 py-[3px]">
-                    <div>
-                        <p class="text-[0.8em] text-left font-medium">
-                            ta
-                    </div>
-                </li>
-                <li class="col-span-1 py-[3px]">
-                    <div>
-                        <p class="text-[0.8em] text-left font-medium">
-                            arce
-                    </div>
-                </li>
-                <li class="col-span-1 py-[3px]">
-                    <div>
-                        <p class="text-[0.8em] text-left font-bold">
-                            queen
-                        </p>
-                    </div>
-                </li>
-                <li class="col-span-1 py-[3px]">
-                    <div>
-                        <p class="text-[0.8em] text-center fot-bold">
-                            maraiah
-                        </p>
-                    </div>
-                </li>
-                <li class="col-span-1 py-[3px]">
-                    <div>
-                        <p class="text-[0.8em] text-center fot-bold">
-                            aiah
-                        </p>
-                    </div>
-                </li>
+                    <li class="col-span-1 py-[3px]">
+                        <div>
+                            <p class="text-[0.8em] text-left font-medium">
+                                {{ $credit->credit_number }}
+                        </div>
+                    </li>
+                    <li class="col-span-1 py-[3px]">
+                        <div>
+                            <p class="text-[0.8em] text-left font-medium">
+                                {{ $credit->customerJoin->firstname . ' ' . $credit->customerJoin->middlename . ' ' . $credit->customerJoin->lastname }}
+                        </div>
+                    </li>
+                    <li class="col-span-1 py-[3px]">
+                        <div>
+                            <p class="text-[0.8em] text-left font-bold">
+                                {{ $credit->customerJoin->contact_number }}
+                            </p>
+                        </div>
+                    </li>
+                    <li class="col-span-1 py-[3px]">
+                        <div>
+                            <p class="text-[0.8em] text-center fot-bold">
+                                {{ $credit->transactionJoin->created_at }}
+                            </p>
+                        </div>
+                    </li>
+                    <li class="col-span-1 py-[3px]">
+                        <div>
+                            <p class="text-[0.8em] text-center fot-bold">
+                                {{ $credit->credit_amount }}
+                            </p>
+                        </div>
+                    </li>
 
-                <li class="col-span-1 py-[3px]">
-                    <div>
-                        <p class="text-[0.8em] text-center fot-bold">
-                            hi
-                        </p>
-                    </div>
-                </li>
-                <li class="col-span-1 py-[3px]">
-                    <div>
-                        <p class="text-[0.8em] text-center fot-bold">
-                            aiah
-                        </p>
-                    </div>
-                </li>
+                    <li class="col-span-1 py-[3px]">
+                        <div>
+                            <p class="text-[0.8em] text-center fot-bold">
+                                {{ $credit->credit_limit }}
+                            </p>
+                        </div>
+                    </li>
 
-            </ul>
-            {{-- @endforeach --}}
+
+                </ul>
+            @endforeach
         </div>
     </div>
 </div>
