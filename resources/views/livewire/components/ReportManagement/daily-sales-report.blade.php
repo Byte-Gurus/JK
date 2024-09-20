bacdc4802935af14ded9c22fcdcbf36e0df6452b
darealZoid <darealZoid@gmail.com>
2024-09-21 00:37:04 +0800

    z

--- resources/views/livewire/components/ReportManagement/daily-sales-report.blade.php
+++ resources/views/livewire/components/ReportManagement/daily-sales-report.blade.php
@@ -131,13 +131,8 @@
                         </li>
                         <li class="col-span-1 py-[3px]">
                             <div>
-<<<<<<< Updated upstream
-                                <p class="text-[0.8em] text-left font-bold">
-                                    {{ $transaction->created_at->format('h:i A') }}
-=======
                                 <p class="text-[0.8em] text-center font-bold">
                                     {{ $transaction->created_at->format('H:i:s') }}
->>>>>>> Stashed changes
                                 </p>
                             </div>
                         </li>
