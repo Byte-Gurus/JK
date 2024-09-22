bbb438f15b709f83f52ff166d046f4b7c4235582
Aceboy76 <harthpalaras62@gmail.com>
2024-09-22 11:03:22 +0800

    asas

--- app/Livewire/Components/CustomerManagement/CustomerForm.php
+++ app/Livewire/Components/CustomerManagement/CustomerForm.php
@@ -86,7 +86,7 @@ class CustomerForm extends Component
 
         $validated = $data['inputAttributes'];
         if ($this->id_picture) {
-            $validated['id_picture'] = $this->id_picture->store('s3');
+            $validated['id_picture'] = $this->id_picture->store('public');
         } else {
             $validated['id_picture'] = null; // or provide a default value if necessary
         }
