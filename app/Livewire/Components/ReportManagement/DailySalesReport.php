f7dc774e8596252a4b06d44fdf2dbbebc1399929
Aceboy76 <harthpalaras62@gmail.com>
2024-09-24 18:22:33 +0800

    asa

--- app/Livewire/Components/ReportManagement/DailySalesReport.php
+++ app/Livewire/Components/ReportManagement/DailySalesReport.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Components\ReportManagement;
 
 use App\Livewire\Pages\ReportManagement;
 use App\Models\Transaction;
+use App\Models\TransactionMovement;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
@@ -27,19 +28,27 @@ class DailySalesReport extends Component
     {
 
         $date = Carbon::parse($date);
-        $this->transactions = Transaction::whereDate('created_at', $date)->get();
+        $this->transactions = TransactionMovement::whereDate('created_at', $date)->get();
 
         $totalGross = 0;
         $totalTax = 0;
         $totalNet = 0;
 
-        foreach($this->transactions as $transaction){
-            $totalGross += $transaction['total_amount'];
-            $totalTax += $transaction['total_vat_amount'];
+        foreach ($this->transactions as $transaction) {
+
+            if ($this->transactions->movement_type == 'Return') {
+                $creditTotalGross = $transaction['returnsJoin']['transactionJoin']['return_total_amount'] * -1;
+            }
+
+
+            $totalGross += $transaction['transactionJoin']['total_amount'] ?? $transaction['creditJoin']['transactionJoin']['total_amount'] ?? $creditTotalGross;
+
+            $totalTax += $transaction['transactionJoin']['total_vat_amount'] ?? $transaction['creditJoin']['transactionJoin']['total_vat_amount'] ?? $transaction['returnsJoin']['transactionJoin']['total_vat_amount'];
 
         }
 
-        $totalNet = $totalGross  -  $totalTax;
+
+        $totalNet = $totalGross - $totalTax;
 
         $this->transaction_info = [
 
@@ -47,7 +56,7 @@ class DailySalesReport extends Component
             'totalTax' => $totalTax,
             'date' => $date->format('M d Y '),
             'totalNet' => $totalNet,
-            'dateCreated' => Carbon::now()->format('M d Y h:i A') ,
+            'dateCreated' => Carbon::now()->format('M d Y h:i A'),
             'createdBy' => Auth::user()->firstname . ' ' . (Auth::user()->middlename ? Auth::user()->middlename . ' ' : '') . Auth::user()->lastname
         ];
 
