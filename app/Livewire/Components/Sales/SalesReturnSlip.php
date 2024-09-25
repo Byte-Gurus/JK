da9d1791e6fdd18d097466d3daef5afd4b2bb0dd
Aceboy76 <harthpalaras62@gmail.com>
2024-09-26 00:22:27 +0800

    Reapply "Merge branch 'production' of https://github.com/darealZoid/JK into production"
    This reverts commit 17e8d687e0cf17dea41021084328ca88a7adb588.

--- app/Livewire/Components/Sales/SalesReturnSlip.php
+++ app/Livewire/Components/Sales/SalesReturnSlip.php
@@ -8,6 +8,6 @@ class SalesReturnSlip extends Component
 {
     public function render()
     {
-        return view('livewire.components.Sales.sales-return-slip');
+        return view('livewire.components.sales.sales-return-slip');
     }
 }
