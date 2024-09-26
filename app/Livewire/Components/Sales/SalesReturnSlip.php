c25e7899f4e0f342f9305e541c4126fe74216aec
Aceboy76 <harthpalaras62@gmail.com>
2024-09-26 21:56:04 +0800

    asa

--- app/Livewire/Components/Sales/SalesReturnSlip.php
+++ app/Livewire/Components/Sales/SalesReturnSlip.php
@@ -14,11 +14,14 @@ class SalesReturnSlip extends Component
     public $return_details, $dateCreated, $return_number, $transaction_number, $transaction_date, $user, $item_return_amount;
     public function render()
     {
-        $this->return_details = ReturnDetails::where('return_id', $this->return_id)->get();
+        if ($this->return_id) {
+            $this->return_details = ReturnDetails::where('return_id', $this->return_id)->get();
+
+        }
         return view('livewire.components.Sales.sales-return-slip', [
             'return_details' => $this->return_details
         ]);
-}
+    }
 
     protected $listeners = [
         'get-return-details' => 'getReturnDetails'
@@ -33,12 +36,13 @@ class SalesReturnSlip extends Component
             'return_number' => $returns->return_number,
             'transaction_number' => $returns->transactionJoin->transaction_number,
             'transaction_date' => $returns->transactionJoin->created_at,
-            'user' =>  Auth::user()->firstname . ' ' . (Auth::user()->middlename ? Auth::user()->middlename . ' ' : '') . Auth::user()->lastname,
+            'user' => Auth::user()->firstname . ' ' . (Auth::user()->middlename ? Auth::user()->middlename . ' ' : '') . Auth::user()->lastname,
             'item_return_amount' => $returns->return_total_amount
 
         ]);
     }
-    public function getReturnDetails($return_id){
+    public function getReturnDetails($return_id)
+    {
 
         $this->return_id = $return_id;
         $this->populateForm();
