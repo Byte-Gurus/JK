a927ab6c7fa74da057d4e39f78ae2c626572cc8a
Aceboy76 <harthpalaras62@gmail.com>
2024-09-26 14:31:40 +0800

    asa

--- app/Livewire/Components/Sales/SalesReturnDetails.php
+++ app/Livewire/Components/Sales/SalesReturnDetails.php
@@ -193,16 +193,16 @@ class SalesReturnDetails extends Component
                 }
 
 
-                $this->return_info[$index] = [
-                    'item_return_amount' => $this->item_return_amount,
-                    'return_quantity' => $this->returnQuantity[$index],
-                    'description' => $this->description[$index] ?? '',
-                    'transaction_details_id' => $transactionDetail->id,
-                    'item_id' => $transactionDetail->item_id,
-                    'inventory_id' => $transactionDetail->inventory_id,
-                    'operation' => $this->operation[$index]
+                // $this->return_info[$index] = [
+                //     'item_return_amount' => $this->item_return_amount,
+                //     'return_quantity' => $this->returnQuantity[$index],
+                //     'description' => $this->description[$index] ?? '',
+                //     'transaction_details_id' => $transactionDetail->id,
+                //     'item_id' => $transactionDetail->item_id,
+                //     'inventory_id' => $transactionDetail->inventory_id,
+                //     'operation' => $this->operation[$index]
 
-                ];
+                // ];
             }
         }
 
