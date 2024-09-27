aec6f101d2fd1eea5fcdeb19234669b85903d1d0
Aceboy76 <harthpalaras62@gmail.com>
2024-09-27 21:32:13 +0800

    asa

--- app/Livewire/Components/Sales/SalesReturnDetails.php
+++ app/Livewire/Components/Sales/SalesReturnDetails.php
@@ -165,21 +165,27 @@ class SalesReturnDetails extends Component
                     $vat_Percent = $transactionDetail->itemJoin->vat_percent;
                     $vatable_return_total_amount = $vatable_Return_Subtotal - ($this->item_return_amount / (100 + $vat_Percent) * 100);
 
-                    dd($vatable_Return_Subtotal,
-                    $vat_Percent,
-                    $vatable_return_total_amount,
-                    $vat_Percent,
-                    $this->item_return_amount);
+                    dd(
+                        $vatable_Return_Subtotal,
+                        $vat_Percent,
+                        $vatable_return_total_amount,
+                        $vat_Percent,
+                        $this->item_return_amount,
+                        $total_vat = $non_vatable_return_total_amount + $vatable_return_total_amount
+                    );
                 } elseif ($transactionDetail->vat_type === 'Non Vatable') {
                     $non_vatable_Return_Subtotal += $this->item_return_amount;
                     $vat_Percent = $transactionDetail->itemJoin->vat_percent;
                     $non_vatable_return_total_amount = $vatable_Return_Subtotal - ($this->item_return_amount / (100 + $vat_Percent) * 100);
 
-                    dd($non_vatable_Return_Subtotal,
-                    $vat_Percent,
-                    $non_vatable_return_total_amount,
-                    $vat_Percent,
-                    $this->item_return_amount);
+                    dd(
+                        $non_vatable_Return_Subtotal,
+                        $vat_Percent,
+                        $non_vatable_return_total_amount,
+                        $vat_Percent,
+                        $this->item_return_amount,
+                        $total_vat = $non_vatable_return_total_amount + $vatable_return_total_amount
+                    );
                 }
 
 
