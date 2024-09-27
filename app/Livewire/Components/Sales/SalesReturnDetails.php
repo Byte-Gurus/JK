ecc64445b42f203c5b54c1f1a608968fa59a4d1c
Aceboy76 <harthpalaras62@gmail.com>
2024-09-27 21:37:41 +0800

    asa

--- app/Livewire/Components/Sales/SalesReturnDetails.php
+++ app/Livewire/Components/Sales/SalesReturnDetails.php
@@ -176,7 +176,7 @@ class SalesReturnDetails extends Component
                 } elseif ($transactionDetail->vat_type === 'Non Vatable') {
                     $non_vatable_Return_Subtotal += $this->item_return_amount;
                     $vat_Percent = $transactionDetail->itemJoin->vat_percent;
-                    $non_vatable_return_total_amount = $vatable_Return_Subtotal - ($this->item_return_amount / (100 + $vat_Percent) * 100);
+                    $non_vatable_return_total_amount = $non_vatable_Return_Subtotal - ($this->item_return_amount / (100 + $vat_Percent) * 100);
 
                     dd(
                         $non_vatable_Return_Subtotal,
