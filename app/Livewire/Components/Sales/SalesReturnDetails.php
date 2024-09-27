18f4702c577555698f24c02b2bfa635c871b2869
Aceboy76 <harthpalaras62@gmail.com>
2024-09-27 21:22:58 +0800

    asa

--- app/Livewire/Components/Sales/SalesReturnDetails.php
+++ app/Livewire/Components/Sales/SalesReturnDetails.php
@@ -156,11 +156,12 @@ class SalesReturnDetails extends Component
                 }
 
                 if ($this->returnQuantity[$index] >= $transactionDetail->itemJoin->bulk_quantity) {
-                    $this->return_total_amount -= $transactionDetail->item_discount_amount;
+                    $this->item_return_amount -= $transactionDetail->item_discount_amount;
                 }
+                dd( $this->item_return_amount, $this->return_total_amount);
 
                 if ($transactionDetail->vat_type === 'Vat') {
-                    $vatable_Return_Subtotal +=  $this->return_total_amount;
+                    $vatable_Return_Subtotal += $transactionDetail->inventoryJoin->selling_price;
                     $vat_Percent = $transactionDetail->itemJoin->vat_percent;
                     $vatable_return_total_amount = $vatable_Return_Subtotal - ($this->item_return_amount / (100 + $vat_Percent) * 100);
 
@@ -170,7 +171,7 @@ class SalesReturnDetails extends Component
                     $vat_Percent,
                     $this->item_return_amount);
                 } elseif ($transactionDetail->vat_type === 'Non Vatable') {
-                    $non_vatable_Return_Subtotal += $this->return_total_amount;
+                    $non_vatable_Return_Subtotal += $this->item_return_amount;
                     $vat_Percent = $transactionDetail->itemJoin->vat_percent;
                     $non_vatable_return_total_amount = $vatable_Return_Subtotal - ($this->item_return_amount / (100 + $vat_Percent) * 100);
 
