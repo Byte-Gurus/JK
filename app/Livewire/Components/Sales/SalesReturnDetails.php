876ad8d9e1a2e56b192e9531f2bf56e565121ff9
Aceboy76 <harthpalaras62@gmail.com>
2024-09-30 13:45:07 +0800

    as

--- app/Livewire/Components/Sales/SalesReturnDetails.php
+++ app/Livewire/Components/Sales/SalesReturnDetails.php
@@ -161,7 +161,7 @@ class SalesReturnDetails extends Component
                     if($transactionDetail->discount_id == 3){
                         $this->item_return_amount =  $this->item_return_amount - ($this->item_return_amount * $transactionDetail->discountJoin->percentage);
                     }
-                    dump($transactionDetail->discount_id);
+                    dump($transactionDetail);
 
                     $this->return_total_amount += $this->item_return_amount;
 
