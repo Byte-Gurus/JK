6f6e74dbd20246044a371d622a10b791e66e5622
Aceboy76 <harthpalaras62@gmail.com>
2024-09-30 14:32:49 +0800

    asa

--- app/Livewire/Components/Sales/SalesReturnDetails.php
+++ app/Livewire/Components/Sales/SalesReturnDetails.php
@@ -156,9 +156,9 @@ class SalesReturnDetails extends Component
 
                     $this->item_return_amount = $this->returnQuantity[$index] * $transactionDetail->inventoryJoin->selling_price;
 
-                    // if ($this->returnQuantity[$index] >= $transactionDetail->itemJoin->bulk_quantity) {
-                    //     $this->item_return_amount -= $transactionDetail->item_discount_amount;
-                    // }
+                    if ($transactionDetail->discount_id == 3) {
+                        $this->item_return_amount = $this->item_return_amount - ($transactionDetail->inventoryJoin->selling_price * ($transactionDetail->discountJoin->percentage / 100));
+                    }
 
                     $this->return_total_amount += $this->item_return_amount;
 
