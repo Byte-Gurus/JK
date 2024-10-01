d66ad2413e00e0a60763e7e5522d53505c101dcf
Aceboy76 <harthpalaras62@gmail.com>
2024-10-01 11:41:28 +0800

    asa

--- app/Livewire/Components/Sales/SalesReturnDetails.php
+++ app/Livewire/Components/Sales/SalesReturnDetails.php
@@ -201,7 +201,7 @@ class SalesReturnDetails extends Component
     protected function validateForm()
     {
         foreach ($this->transactionDetails as $index => $transactionDetail) {
-            if (isset($this->returnQuantity[$index])  && $this->returnQuantity[$index] ) {
+            if (isset($this->returnQuantity[$index])  && $this->returnQuantity[$index] && $this->returnQuantity[$index] != null) {
                 $availableQty = $transactionDetail['item_quantity'];
                 $this->rules["returnQuantity.$index"] = ['required', 'numeric', 'min:1', "lte:$availableQty"];
             }
