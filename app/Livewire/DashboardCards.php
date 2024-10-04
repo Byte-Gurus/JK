8f5d22e2081e0988a850151069bfe8dbc1c13723
Aceboy76 <harthpalaras62@gmail.com>
2024-10-04 13:06:41 +0800

    Asa

--- app/Livewire/DashboardCards.php
+++ app/Livewire/DashboardCards.php
@@ -10,11 +10,16 @@ use Livewire\Component;
 
 class DashboardCards extends Component
 {
-    public $overallSales, $overallStocks, $overallReturn,$deliveryInProgress;
+    public $overallSales, $overallStocks, $overallReturn, $deliveryInProgress;
     public function render()
     {
 
-        $this->overallSales = Transaction::sum('total_amount');
+        $this->overallSales = Transaction::whereNotIn('transaction_type', ['Return', 'Void'])
+            ->whereDoesntHave('transactionDetailsJoin', function ($query) {
+                $query->whereIn('status', ['Void', 'Return']);
+            })
+            ->sum('total_amount');
+
         $this->overallStocks = Inventory::where('status', 'Available')->sum('current_stock_quantity');
         $this->overallReturn = ReturnDetails::sum('return_quantity');
         $this->deliveryInProgress = Delivery::where('status', 'In Progress')->count();
