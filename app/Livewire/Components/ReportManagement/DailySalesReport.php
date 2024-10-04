d13ddfcb61853463cc2069adaed2a6c61aba33f2
Aceboy76 <harthpalaras62@gmail.com>
2024-10-05 02:26:35 +0800

    Asa

--- app/Livewire/Components/ReportManagement/DailySalesReport.php
+++ app/Livewire/Components/ReportManagement/DailySalesReport.php
@@ -196,7 +196,7 @@ class DailySalesReport extends Component
     {
         if ($detail->status == 'Void') {
 
-
+            $transaction->totalVoidItemAmount +=  $detail->item_subtotal;
             if ($detail->vat_type === 'Vat') {
                 $vatable_subtotal = $detail->item_subtotal;
                 $vatable_amount = $vatable_subtotal - ($vatable_subtotal / (100 + $detail->item_vat_percent) * 100);
