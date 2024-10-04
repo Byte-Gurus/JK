37283eefa1b1b371e59399c6c43d063d32486027
Aceboy76 <harthpalaras62@gmail.com>
2024-10-05 00:49:48 +0800

    Asa

--- app/Livewire/Components/ReportManagement/DailySalesReport.php
+++ app/Livewire/Components/ReportManagement/DailySalesReport.php
@@ -68,7 +68,7 @@ class DailySalesReport extends Component
                         if ($detail->status == 'Void') {
                             $transaction->totalVoidItemAmount += $detail->item_subtotal;
                         }
-
+                        dd($detail);
                         $transaction->VoidTaxAmount = $this->calculateVoidAmounts($detail, $transaction);
                     }
                     break;
