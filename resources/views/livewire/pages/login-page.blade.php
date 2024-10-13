e15653200a44b8b2e7d2feb530fb4d5845ca813d
darealZoid <darealZoid@gmail.com>
2024-10-13 18:10:47 +0800

    z

--- resources/views/livewire/pages/login-page.blade.php
+++ resources/views/livewire/pages/login-page.blade.php
@@ -30,40 +30,41 @@
                     @enderror
                 </div>
 
-                <div class="flex flex-col mb-12" x-data="{ showPassword: @entangle('showPassword') }">
+                <div class="relative flex flex-col mb-12" x-data="{ showPassword: @entangle('showPassword') }">
                     <label for="password" class="text-[rgb(53,53,53)]">Password</label>
-                    <input :type="showPassword ? 'password' : 'text'" id="password" name="password" required
-                        class="w-full py-2 pl-4 pr-12 text-sm border-2 border-black rounded-md outline-none bg-gray-50 text-none focus:outline-none"
-                        wire:model="password">
+                    <div class="relative items-center">
+                        <input :type="showPassword ? 'password' : 'text'" id="password" name="password" required
+                            class="absolute w-full py-2 pl-4 pr-12 text-sm border-2 border-black rounded-md outline-none bg-gray-50 text-none focus:outline-none"
+                            wire:model="password">
 
-                    @if ($this->showPassword)
-                        <p x-cloak class="fixed self-end mt-8 mr-4 cursor-pointer "
-                            x-on:click=" $wire.showPasswordStatus()">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                stroke-width="1.5" stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
-                            </svg>
-                        </p>
-                    @else
-                        <p class="fixed self-end mt-8 mr-4 cursor-pointer "
-                            x-on:click=" $wire.showPasswordStatus()">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                stroke-width="1.5" stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                            </svg>
-                        </p>
-                    @endif
+                        @if ($this->showPassword)
+                            <p x-cloak class="absolute right-0 mt-2 mr-4" x-on:click=" $wire.showPasswordStatus()">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                                </svg>
+                            </p>
+                        @else
+                            {{-- <p class="relative self-end mt-8 mr-4 cursor-pointer " --}}
+                            <p class="absolute right-0 mt-2 mr-4" x-on:click=" $wire.showPasswordStatus()">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                </svg>
+                            </p>
+                        @endif
+                    </div>
 
                     @error('password')
                         <span class="mt-2 font-medium text-red-500 text-md">{{ $message }}</span>
                     @enderror
                 </div>
 
-                <div class="text-right">
+                <div class="mt-8 text-right">
                     <button type="submit" wire:loading.remove
                         class="w-full px-8 py-2 font-bold outline-none focus:outline-none text-[rgb(249,249,249)] bg-[rgb(53,53,53)] border-2 border-black rounded-md text-md hover:bg-[rgb(80,80,80)]">
                         Log In
