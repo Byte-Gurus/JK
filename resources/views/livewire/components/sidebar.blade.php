7e9e9e2845b3d89dcc9f70451ef9e7ef12c201c2
darealZoid <darealZoid@gmail.com>
2024-10-15 01:17:00 +0800

    z

--- resources/views/livewire/components/sidebar.blade.php
+++ resources/views/livewire/components/sidebar.blade.php
@@ -93,7 +93,7 @@
                 </a>
             </li>
             <li>
-                <a wire:navigate href="{{ route('cashierpage.index') }}"
+                <a href="{{ route('cashierpage.index') }}"git
                     class="w-full bg-transparent text-sm font-medium text-[#EFEFEF] hover:bg-[#464646] px-6 py-2 flex flex-row gap-4 items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-6 h-6">
