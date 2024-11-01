8047de3efcf034772cca6c12ee89595d6c1451d0
Aceboy76 <harthpalaras62@gmail.com>
2024-11-02 00:18:30 +0800

    as

--- app/Livewire/Components/CustomerManagement/CustomerForm.php
+++ app/Livewire/Components/CustomerManagement/CustomerForm.php
@@ -322,13 +322,14 @@ class CustomerForm extends Component
 
         ];
 
+        $isValidated = true;
         if ($this->customertype == 'Senior Citizen') {
             $rules['birthdate'] = 'required|date|before_or_equal:' . now()->subYears(60)->toDateString();
             $rules['senior_pwd_id'] = 'numeric';
 
             if (strlen((string) $this->senior_pwd_id) < 4) {
                 $this->addError('senior_pwd_id', 'The minimum digits for Senior Citizen ID is 4');
-                return;
+                $isValidated = false;
             }
         } elseif ($this->customertype == 'PWD') {
             $rules['birthdate'] = 'required|date|before_or_equal:' . now()->subYears(18)->toDateString();
@@ -337,17 +338,20 @@ class CustomerForm extends Component
 
             if (strlen((string) $this->senior_pwd_id) < 7) {
                 $this->addError('senior_pwd_id', 'The minimum digits for PWD ID is 7');
-                return;
+                $isValidated = false;
             }
         } elseif ($this->customertype == 'Normal') {
             $rules['birthdate'] = 'required|date|before_or_equal:' . now()->subYears(18)->toDateString();
             $rules['senior_pwd_id'] = 'nullable|string|max:255';
         }
 
+        if($isValidated == true){
+            return $this->validate($rules);
+        }
 
 
 
-        return $this->validate($rules);
+
     }
 
     public function resetForm() //*tanggalin ang laman ng input pati $user_id value
--- app/Livewire/Components/Sales/DiscountForm.php
+++ app/Livewire/Components/Sales/DiscountForm.php
@@ -243,19 +243,20 @@ class DiscountForm extends Component
                 'customerType' => 'required|in:PWD,Senior Citizen',
             ];
 
+            $isValidated = true;
             if ($this->customerType == 'Senior Citizen') {
                 $rules['birthdate'] = 'required|date|before_or_equal:' . now()->subYears(60)->toDateString();
 
                 if (strlen((string) $this->senior_pwd_id) < 4) {
                     $this->addError('senior_pwd_id', 'The minimum digits for Senior Citizen ID is 4');
-                    return;
+                    $isValidated = false;
                 }
             } elseif ($this->customerType == 'PWD') {
                 $rules['birthdate'] = 'required|date|before_or_equal:' . now()->subYears(18)->toDateString();
 
                 if (strlen((string) $this->senior_pwd_id) < 7) {
                     $this->addError('senior_pwd_id', 'The minimum digits for PWD ID is 7');
-                    return;
+                    $isValidated = false;
                 }
             }
         } else {
@@ -274,7 +275,10 @@ class DiscountForm extends Component
 
 
 
-        return $this->validate($rules);
+        if ($isValidated == true) {
+            return $this->validate($rules);
+        }
+
     }
 
     public function createCustomer()
