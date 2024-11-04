c3bee45b2637a32d7808b8f68343988614d11d62
Aceboy76 <harthpalaras62@gmail.com>
2024-11-04 10:31:56 +0800

    as

--- routes/console.php
+++ routes/console.php
@@ -6,6 +6,7 @@ use App\Models\Notification;
 use Carbon\Carbon;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
