4ea59a25ccaac89520ad53e93b926f315acf208b
darealZoid <darealZoid@gmail.com>
2024-11-06 17:16:08 +0800

    z

--- resources/views/livewire/pages/logs.blade.php
+++ resources/views/livewire/pages/logs.blade.php
@@ -1,3 +1,32 @@
-<div>
-    
+<div class="flex justify-center">
+    <div class="flow-root">
+        <ul role="list" class="-mb-8">
+            <li>
+                <div class="relative pb-8">
+                    <span class="absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"></span>
+                    <div class="relative flex space-x-3">
+                        <div>
+                            <span
+                                class="flex items-center justify-center w-8 h-8 bg-gray-400 rounded-full ring-8 ring-white">
+                                <svg class="w-5 h-5 text-white" viewBox="0 0 20 20" fill="currentColor"
+                                    aria-hidden="true" data-slot="icon">
+                                    <path
+                                        d="M10 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM3.465 14.493a1.23 1.23 0 0 0 .41 1.412A9.957 9.957 0 0 0 10 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 0 0-13.074.003Z" />
+                                </svg>
+                            </span>
+                        </div>
+                        <div class="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
+                            <div>
+                                <p class="text-sm text-gray-500">Applied to <a href="#"
+                                        class="font-medium text-gray-900">Front End Developer</a></p>
+                            </div>
+                            <div class="text-sm text-right text-gray-500 whitespace-nowrap">
+                                <time datetime="2020-09-20">Sep 20</time>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
 </div>
