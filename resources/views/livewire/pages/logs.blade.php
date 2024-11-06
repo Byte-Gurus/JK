e12b00c4724bf18c1da2fe3d20f2bfde016772a8
darealZoid <darealZoid@gmail.com>
2024-11-06 22:31:35 +0800

    z

--- resources/views/livewire/pages/logs.blade.php
+++ resources/views/livewire/pages/logs.blade.php
@@ -50,9 +50,12 @@
                                     <div>
                                         <span
                                             class="flex items-center justify-center w-8 h-8 bg-[rgb(47,34,11)] rounded-full ring-4 ring-[rgb(56,42,13)]">
-                                            <svg class="text-orange-700 " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-                                                <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
-                                              </svg>
+                                            <svg class="text-orange-700 " xmlns="http://www.w3.org/2000/svg"
+                                                fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                                stroke="currentColor" class="size-4">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                                            </svg>
 
                                         </span>
                                     </div>
@@ -112,9 +115,9 @@
                 @endforeach
             </ul>
         </div>
-        <div class="absolute gap-6 mx-4 my-2 bottom-0self-center justify-evenly text-nowrap">
-            {{ $logs->links() }}
+    </div>
+    <div class="py-4 ">
+        {{ $logs->links() }}
 
-        </div>
     </div>
 </div>
