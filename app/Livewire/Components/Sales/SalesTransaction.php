f42cfa292aeae8f2c45fdd33f9f44d386a4c62dd
Aceboy76 <harthpalaras62@gmail.com>
2024-11-07 10:32:27 +0800

    as

--- app/Livewire/Components/Sales/SalesTransaction.php
+++ app/Livewire/Components/Sales/SalesTransaction.php
@@ -413,10 +413,10 @@ class SalesTransaction extends Component
             $newGrandTotal = $this->subtotal - ($this->selectedItems[$this->selectedIndex]['total_amount'] ?? 0);
 
 
-            if ($newGrandTotal < $this->subtotal - $this->transactionDiscount - $this->excess_amount) {
-                $this->alert('error', 'Subtotal must not be below exchange amount');
-                return;
-            }
+            // if ($newGrandTotal < $this->subtotal - $this->transactionDiscount - $this->excess_amount) {
+            //     $this->alert('error', 'Subtotal must not be below exchange amount');
+            //     return;
+            // }
 
 
             if (
