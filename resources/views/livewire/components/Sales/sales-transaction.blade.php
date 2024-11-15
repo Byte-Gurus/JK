42f843cbdd8d9023182e46ed706842f5a6a81d58
darealZoid <darealZoid@gmail.com>
2024-11-15 23:31:57 +0800

    z

--- resources/views/livewire/components/Sales/sales-transaction.blade.php
+++ resources/views/livewire/components/Sales/sales-transaction.blade.php
@@ -452,7 +452,7 @@
                     @if (!empty($credit_no))
                         <div wire:click='clearSelectedCustomerName()' class="flex justify-end">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                strokeWidth={1.5} stroke="currentColor" class="size-6">
+                                strokeWidth={1.5} stroke="currentColor" class="text-red-500 size-8">
                                 <path strokeLinecap="round" strokeLinejoin="round"
                                     d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                             </svg>
