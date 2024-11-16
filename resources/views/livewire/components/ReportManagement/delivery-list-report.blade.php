b37a336e3b477aaa60dbc738a11b3762ab294d8d
darealZoid <darealZoid@gmail.com>
2024-11-16 16:15:51 +0800

    z

--- resources/views/livewire/components/ReportManagement/delivery-list-report.blade.php
+++ resources/views/livewire/components/ReportManagement/delivery-list-report.blade.php
@@ -16,21 +16,45 @@
                 </div>
             </div>
         </div>
-        <div class="grid grid-flow-col ">
-            <div>
-                <p class="text-[1.4em] self-end font-bold text-right italic m-4 mr-10 uppercase">DELIVERY LIST REPORT
-                </p>
+        {{-- @if ($deliverylist_info) --}}
+            <div class="grid items-center grid-flow-col grid-cols-2 ">
+                <div class="flex flex-col justify-between col-span-1 px-4">
+                    <div class="flex flex-col ">
+                        <div class="flex flex-row gap-2 text-nowrap">
+                            <p class="text-[1em] font-black uppercase">Report as of</p>
+                            <p class="text-[1em] font-medium uppercase">{{ $date ?? ' ' }}</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <p class="text-[1.4em] font-bold text-right italic m-4 mr-10 uppercase">DELIVERY LIST REPORT</p>
+                </div>
             </div>
-        </div>
+        {{-- @endif --}}
 
         <div>
             <div class="w-full my-4 border-b border-black"> </div>
 
-            <ul class="grid justify-between grid-flow-col grid-cols-3 mx-4 ">
+            <ul class="grid justify-between grid-flow-col grid-cols-6 mx-4 ">
 
                 <li class="col-span-1 ">
                     <div>
-                        <p class="text-[1em] uppercase text-left font-bold">Date Created</p>
+                        <p class="text-[1em] uppercase text-left font-bold">PO Date</p>
+                    </div>
+                </li>
+                <li class="col-span-1 ">
+                    <div>
+                        <p class="text-[1em] uppercase text-left font-bold">PO #</p>
+                    </div>
+                </li>
+                <li class="col-span-1 ">
+                    <div>
+                        <p class="text-[1em] uppercase text-left font-bold">Delivery ID</p>
+                    </div>
+                </li>
+                <li class="col-span-1 ">
+                    <div>
+                        <p class="text-[1em] uppercase text-left font-bold">Delivery Receipt No.</p>
                     </div>
                 </li>
                 <li class="col-span-1 ">
@@ -48,37 +72,37 @@
 
             <div cwlass="w-full my-4 border-b border-black"> </div>
             @if (!$deliveries)
-            <p class="w-full my-8 text-center text-[2em] font-black opacity-30">NO DELIVERY FOUND AVAILABLE</p>
+                <p class="w-full my-8 text-center text-[2em] font-black opacity-30">NO DELIVERY FOUND AVAILABLE</p>
             @else
-            @foreach ($deliveries as $delivery)
-            <ul class="grid justify-between grid-flow-col grid-cols-3 mx-4 ">
-                <li class="col-span-1 py-[3px]">
-                    <div>
-                        <p class="text-[1em] text-left font-medium">
-                            {{ $delivery->created_at->format('M d Y') }}</p>
-                    </div>
-                </li>
-                <li class="col-span-1 text-left py-[3px]">
-                    <div>
-                        <p class="text-[1em] text-left font-medium">
-                            {{ $delivery->status }}
-                        </p>
-                    </div>
-                </li>
-                <li class="col-span-1 text-left py-[3px]">
-                    <div>
-                        <p class="text-[1em] text-left font-medium">
-                            @if ($delivery->date_delivered == 'N/A')
-                            N/A
-                            @else
-                            {{ \Carbon\Carbon::parse($delivery->date_delivered)->format('M d Y') }}
-                            @endif
+                @foreach ($deliveries as $delivery)
+                    <ul class="grid justify-between grid-flow-col grid-cols-6 mx-4 ">
+                        <li class="col-span-1 py-[3px]">
+                            <div>
+                                <p class="text-[1em] text-left font-medium">
+                                    {{ $delivery->created_at->format('M d Y') }}</p>
+                            </div>
+                        </li>
+                        <li class="col-span-1 text-left py-[3px]">
+                            <div>
+                                <p class="text-[1em] text-left font-medium">
+                                    {{ $delivery->status }}
+                                </p>
+                            </div>
+                        </li>
+                        <li class="col-span-1 text-left py-[3px]">
+                            <div>
+                                <p class="text-[1em] text-left font-medium">
+                                    @if ($delivery->date_delivered == 'N/A')
+                                        N/A
+                                    @else
+                                        {{ \Carbon\Carbon::parse($delivery->date_delivered)->format('M d Y') }}
+                                    @endif
 
-                        </p>
-                    </div>
-                </li>
-            </ul>
-            @endforeach
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
+                @endforeach
             @endif
         </div>
         <div class="px-4 py-4">
