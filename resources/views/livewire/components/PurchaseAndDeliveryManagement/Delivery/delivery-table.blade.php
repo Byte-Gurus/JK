8b380c61a7f184d9ccd5a26e4372c55ded5e7dbe
darealZoid <darealZoid@gmail.com>
2024-11-17 15:41:04 +0800

    z

--- resources/views/livewire/components/PurchaseAndDeliveryManagement/Delivery/delivery-table.blade.php
+++ resources/views/livewire/components/PurchaseAndDeliveryManagement/Delivery/delivery-table.blade.php
@@ -249,11 +249,6 @@
                                     <button
                                         x-on:click="$wire.viewRestockForm(); $wire.getDeliveryID({{ $delivery->id }}); openActions = !openActions"
                                         class="flex flex-row items-center gap-2 px-2 py-0.5 font-bold transition-none ease-in-out bg-blue-100 border border-blue-900 rounded-lg hover:bg-blue-300 duration-0">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="size-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                                        </svg>
                                         <p class="text-sm ">Restock</p>
                                     </button>
                                 @endif
@@ -263,7 +258,7 @@
                                         x-on:click=" $wire.showImage('{{ $delivery->id }}'), openActions = !openActions">
                                         <p class="text-sm hover:text-[rgb(138,117,59)]">Receipt</p>
                                     </div>
-                                    <div class="cursor-pointer hover:bg-[rgb(255,234,174)] rounded-full p-1"
+                                    <div class="cursor-pointer hover:bg-[rgb(174,236,255)] rounded-full p-1"
                                         wire:click="printPO({{ $delivery->id }})"
                                         x-on:click="$wire.displayPrintPurchaseOrderDetails()">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -281,7 +276,7 @@
                                         x-on:click=" $wire.showImage('{{ $delivery->id }}'), openActions = !openActions">
                                         <p class="text-sm hover:text-[rgb(138,117,59)]">Receipt</p>
                                     </div>
-                                    <div class="cursor-pointer hover:bg-[rgb(255,234,174)] rounded-full p-1"
+                                    <div class="cursor-pointer hover:bg-[rgb(174,236,255)] rounded-full p-1"
                                         wire:click="printPO({{ $delivery->id }})"
                                         x-on:click="$wire.displayPrintPurchaseOrderDetails()">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
