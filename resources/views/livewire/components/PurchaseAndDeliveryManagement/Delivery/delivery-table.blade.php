228f810a4fb929434ed0dfa0e571a2ffbecd12af
darealZoid <darealZoid@gmail.com>
2024-11-18 12:41:18 +0800

    z

--- resources/views/livewire/components/PurchaseAndDeliveryManagement/Delivery/delivery-table.blade.php
+++ resources/views/livewire/components/PurchaseAndDeliveryManagement/Delivery/delivery-table.blade.php
@@ -199,7 +199,7 @@
                         <p class="italic font-medium text-md">N/A</p>
                         @elseif ($delivery->status === 'In Progress')
                         <div class="flex flex-row items-center gap-2">
-                            <button x-on:click="$wire.cancelDelivery({{ $delivery->id }}); openActions = !openActions"
+                            <button x-on:click="$wire.cancelDelivery({{ $delivery->id }});"
                                 class="flex items-center p-1.5 transition-all duration-100 ease-in-out rounded-full hover:bg-red-200">
                                 <div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
                                         class="text-red-500 size-6">
@@ -222,7 +222,7 @@
 
                                 @if ($delivery->status == 'Delivered')
                                     <div class="  flex items-center text-[rgb(53,53,53)] transition-all duration-100 ease-in-out cursor-pointer underline"
-                                        x-on:click=" $wire.showImage('{{ $delivery->id }}'), openActions = !openActions">
+                                        x-on:click=" $wire.showImage('{{ $delivery->id }}');">
                                         <p class="text-sm ">Receipt</p>
                                     </div>
                                     <div class="cursor-pointer hover:bg-[rgb(174,236,255)] rounded-full p-1"
@@ -244,7 +244,7 @@
                                         </svg>
                                     </button>
                                     <button
-                                        x-on:click="$wire.viewRestockForm(); $wire.getDeliveryID({{ $delivery->id }}); openActions = !openActions"
+                                        x-on:click="$wire.viewRestockForm(); $wire.getDeliveryID({{ $delivery->id }});"
                                         class="flex flex-row items-center gap-2 px-2 py-0.5 font-bold transition-none ease-in-out bg-blue-100 border border-blue-900 rounded-lg hover:bg-blue-300 duration-0">
                                         <p class="text-sm ">Restock</p>
                                     </button>
@@ -252,7 +252,7 @@
 
                                 @if ($delivery->status === 'Complete Stock in')
                                     <div class="  flex items-center text-[rgb(53,53,53)] transition-all duration-100 ease-in-out cursor-pointer underline"
-                                        x-on:click=" $wire.showImage('{{ $delivery->id }}'), openActions = !openActions">
+                                        x-on:click=" $wire.showImage('{{ $delivery->id }}')">
                                         <p class="text-sm hover:text-[rgb(138,117,59)]">Receipt</p>
                                     </div>
                                     <div class="cursor-pointer hover:bg-[rgb(174,236,255)] rounded-full p-1"
@@ -270,7 +270,7 @@
                                     ($delivery->status === 'Stocked in with backorder' && $delivery->purchaseJoin->backorderJoin->isNotEmpty()) ||
                                         $delivery->status === 'Backorder complete')
                                     <div class="  flex items-center text-[rgb(53,53,53)] transition-all duration-100 ease-in-out cursor-pointer underline"
-                                        x-on:click=" $wire.showImage('{{ $delivery->id }}'), openActions = !openActions">
+                                        x-on:click=" $wire.showImage('{{ $delivery->id }}')">
                                         <p class="text-sm hover:text-[rgb(138,117,59)]">Receipt</p>
                                     </div>
                                     <div class="cursor-pointer hover:bg-[rgb(174,236,255)] rounded-full p-1"
@@ -282,7 +282,7 @@
                                                 d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0 1 10.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0 .229 2.523a1.125 1.125 0 0 1-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0 0 21 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 0 0-1.913-.247M6.34 18H5.25A2.25 2.25 0 0 1 3 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 0 1 1.913-.247m10.5 0a48.536 48.536 0 0 0-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5Zm-3 0h.008v.008H15V10.5Z" />
                                         </svg>
                                     </div>
-                                    <button x-on:click="$wire.viewBackorderDetails(); openActions = !openActions"
+                                    <button x-on:click="$wire.viewBackorderDetails();"
                                         wire:click="getPO_ID({{ $delivery->id }})"
                                         class="flex flex-row items-center gap-2 px-2 py-0.5 font-bold transition-none ease-in-out bg-violet-100 border border-violet-900 rounded-lg hover:bg-violet-300 duration-0">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
